8d2afcc693da06a3cc7b60e85fe70621ddb7ab3b
Anderson_salamanca <anderson.salamancag@konradlorenz.edu.co>
2020-11-29 22:21:27 -0500

    prueba testing

---
+++ PruebaChoucair/src/test/java/com/pruebaChoucair/Prueba.java
@@ -0,0 +1,45 @@
+package com.pruebaChoucair;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.ForkJoinPool.ManagedBlocker;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Prueba {
+	
+	private WebDriver driver;
+	
+	@Before
+	public void SetUp() {
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/chomedriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.choucairtesting.com/empleos-testing/");
+		
+	}
+	
+	@Test 
+	public void testnavbar() {
+		
+		WebElement tag =driver.findElement(By.id("menu-item-6142"));
+		tag.click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		assertEquals("Servicios-Choucair Testing", driver.getTitle());
+		
+		
+	}
+	
+	@After
+	public void terminar() {
+		driver.quit();
+	}
+
+}
